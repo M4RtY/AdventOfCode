b8c35d1c7815237f9e76c8aca6dc4e8233f05577
Martin Kolar <info@kodovani.eu>
2022-12-02 13:08:30 +0100

    Year 2015 - Day02 completed

--- src/main/java/aoc/y2k15/Day02.java
+++ src/main/java/aoc/y2k15/Day02.java
@@ -2,13 +2,14 @@ package aoc.y2k15;
 
 import aoc.utils.TextReader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Day02 {
-   private final List<String> lines;
+   private final List<String> dimensions;
 
    public Day02() {
-      lines = new TextReader().readString("src/main/resources/y2k15/day01.txt");
+      dimensions = new TextReader().readString("src/main/resources/y2k15/day02.txt");
    }
 
    public void run() {
@@ -17,10 +18,42 @@ public class Day02 {
    }
 
    public void part1() {
-      int surface = 0;
+      int l, w, h;
+      int smallest, surface = 0;
+
+      for (String dimension : dimensions) {
+         int[] dimensionNumbers = Arrays.stream(Arrays.stream(dimension.split("x")).mapToInt(Integer::parseInt).toArray()).toArray();
+
+         l = dimensionNumbers[0];
+         w = dimensionNumbers[1];
+         h = dimensionNumbers[2];
+
+         int[] sides = {l*w, w*h, h*l};
+         Arrays.sort(sides);
+
+         smallest = sides[0];
+
+         surface += (2*l*w + 2*w*h + 2*h*l) + smallest;
+      }
+
+      System.out.println("The total square feet of wrapping paper is " + surface);
    }
 
    public void part2() {
+      int l, w, h;
+      int ribbonLength = 0;
 
+      for (String dimension : dimensions) {
+         int[] dimensionNumbers = Arrays.stream(Arrays.stream(dimension.split("x")).mapToInt(Integer::parseInt).toArray()).toArray();
+         Arrays.sort(dimensionNumbers);
+
+         l = dimensionNumbers[0];
+         w = dimensionNumbers[1];
+         h = dimensionNumbers[2];
+
+         ribbonLength += (2*l + 2*w) + (l*w*h);
+      }
+
+      System.out.println("The total feet of ribbon is " + ribbonLength);
    }
 }
